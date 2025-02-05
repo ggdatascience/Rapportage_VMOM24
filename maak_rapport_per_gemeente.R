657a37f7dc2c083ded5f04a73994560f19878227
Velden <j.ten.velden@ggdhvb.nl>
2025-02-05 14:10:25 +0100

    Projectinitialisatie

---
+++ maak_rapport_per_gemeente.R
@@ -0,0 +1,19 @@
+#maak_rapport_gemeenten
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+library(dplyr)
+
+#vector met numerieke waarden gemeentecodes in regio
+gemeentecodes_in_regio = c(2,3,4,5,6) #codes nepgemeenten voorbeeldrapportage
+
+# HTML uitdraai per gemeente -----------------------------------------------------------
+for(gemeentecode in gemeentecodes_in_regio){
+
+  quarto::quarto_render(
+    input = "voorbeeld_rapportage.qmd", # Wijzig dit naar "Rapportage.qmd" als je deze voor meerdere gemeentes wil uitdraaien
+    output_format = "html",
+    output_file = glue::glue("gemeenterapport_{gemeentecode}.html"),
+    execute_params = list(
+      gemeentecode = gemeentecode)
+    )
+  
+}
